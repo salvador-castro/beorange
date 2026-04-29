5a7ae940d899957f0f7604c72a075e8d693d1204
Salva Castro <salvacastro06@gmail.com>
2026-04-29 19:15:15 -0300

    features footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,11 +21,18 @@ export default function Footer() {
             </div>
           </div>
           <div className="w-full md:w-1/2 flex justify-end">
-            <img
-              src="/recursos/monedaConAnteojos.png"
-              alt="Bitcoin Contacto"
-              className="w-48 md:w-72 lg:w-80 drop-shadow-2xl"
-            />
+            <div className="relative">
+              <img
+                src="/recursos/monedaConAnteojos.png"
+                alt="Bitcoin Contacto"
+                className="w-48 md:w-72 lg:w-80 drop-shadow-2xl relative z-10"
+              />
+              <img
+                src="/recursos/sobreEmail.png"
+                alt="Email"
+                className="absolute -bottom-4 -right-8 md:-bottom-8 md:-right-12 w-28 md:w-40 lg:w-48 drop-shadow-2xl z-20 hover:-translate-y-3 hover:scale-105 transition-all duration-300 cursor-pointer"
+              />
+            </div>
           </div>
         </div>
       </section>
