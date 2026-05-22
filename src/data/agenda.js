d0b7e9c37a617e5cd78203bde5f953fc6b3f388d
Salvador Castro <salvacastro06@gmail.com>
2026-05-22 12:45:48 -0300

    update agenda

--- src/data/agenda.js
+++ src/data/agenda.js
@@ -7,11 +7,11 @@ export const AGENDA_ITEMS = [
   {
     time: '15:45',
     title: 'Presentación BeOrange',
-    moderator: 'Yenifer Núñez de Her Chain',
+    moderator: 'Yenifer Núñez',
     speakers: [
-      'Maxi Perez de Plugin',
-      'Rodrigo Castañeda de Roderich’s Crypto House',
-      'Sebastian Contreras de Tangram'
+      'Maxi Perez',
+      'Rodrigo Castañeda',
+      'Sebastian Contreras'
     ]
   },
   {
@@ -23,21 +23,21 @@ export const AGENDA_ITEMS = [
   {
     time: '16:10',
     title: 'Ecosistema y Adopción',
-    moderator: 'Lucia Cardelino de Ethereum Uruguay',
+    moderator: 'Lucia Cardelino',
     speakers: [
-      'Bruno Vacotti de Acelerando Bitcoin',
-      'Martin Benitez de Cripto Guardian',
-      'Ivan Kaleja de B4H'
+      'Bruno Vacotti',
+      'Martin Benitez',
+      'Ivan Kaleja'
     ]
   },
   {
     time: '16:35',
     title: 'RSK: Casos reales de uso',
-    moderator: 'Ignacio Varese de Blockchain Summit',
+    moderator: 'Ignacio Varese',
     speakers: [
-      'Adrian Eidelman de RSK',
-      'Maximiliano Carjuza de Money On Chain',
-      'Jon Bravetti de BALT Protocol'
+      'Adrian Eidelman',
+      'Maximiliano Carjuza',
+      'Jon Bravetti'
     ]
   },
   {
@@ -48,26 +48,26 @@ export const AGENDA_ITEMS = [
   {
     time: '17:30',
     title: 'BTC desde el sistema tradicional',
-    moderator: 'Maxi Perez de Plugin',
-    speakers: ['Nicolas Rodriguez de Club del Inversor']
+    moderator: 'Maxi Perez',
+    speakers: ['Nicolas Rodriguez']
   },
   {
     time: '17:45',
     title: 'Futuro del desarrollador',
-    moderator: 'Guido Iafigliola de Mutuo',
-    speakers: ['Juan Manuel Sobral de Space Dev']
+    moderator: 'Guido Iafigliola',
+    speakers: ['Juan Manuel Sobral']
   },
   {
     time: '18:00',
     title: 'Keynote - Bybit',
     moderator: null,
-    speakers: ['Leandro Fiorenza de ByBit']
+    speakers: ['Leandro Fiorenza']
   },
   {
     time: '18:15',
     title: 'Minería',
-    moderator: 'Rodrigo Castañeda de Roderich’s Crypto House',
-    speakers: ['Fabrizio Bianchi de Hashrate Space']
+    moderator: 'Rodrigo Castañeda',
+    speakers: ['Fabrizio Bianchi']
   },
   {
     time: '18:50',
@@ -77,37 +77,37 @@ export const AGENDA_ITEMS = [
   {
     time: '19:00',
     title: 'Divulgando BTC',
-    moderator: 'Rodolfo Andragnes de Bitcoin.ar-Labitconf',
+    moderator: 'Rodolfo Andragnes',
     speakers: [
-      'Javier Puig de Archivo Cripto',
-      'Joy Koloff de BitcoinNea',
+      'Javier Puig',
+      'Joy Koloff',
       'Pamela Gonzales Perilli'
     ]
   },
   {
     time: '19:25',
     title: 'Regulación',
-    moderator: 'Ricardo Mihura de Bitcoin AR',
+    moderator: 'Ricardo Mihura',
     speakers: [
-      'Carolina Rodriguez de Legaltech solutions',
-      'Juan Diana de Brum Costa Abogados',
+      'Carolina Rodriguez',
+      'Juan Diana',
       'Ximena Gambaro'
     ]
   },
   {
     time: '19:50',
     title: 'Entrevista BTC en LATAM',
-    moderator: 'Sebastian Contreras de Tangram',
-    speakers: ['Rodolfo Andragnes de Bitcoin.ar-LABITCONF']
+    moderator: 'Sebastian Contreras',
+    speakers: ['Rodolfo Andragnes']
   },
   {
     time: '20:05',
     title: 'Cierre y Sorteo',
-    moderator: 'Yenifer Núñez de Her Chain',
+    moderator: 'Yenifer Núñez',
     speakers: [
-      'Maxi Perez de Plugin',
-      'Rodrigo Castañeda de Roderich’s',
-      'Sebastian Contreras de Tangram'
+      'Maxi Perez',
+      'Rodrigo Castañeda',
+      'Sebastian Contreras'
     ]
   },
   {
