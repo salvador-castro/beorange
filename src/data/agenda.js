90b12ea02195641ccd8ffec0ac2dbad48a482280
Salvador Castro <salvacastro06@gmail.com>
2026-05-22 12:21:04 -0300

    update agenda

--- src/data/agenda.js
+++ src/data/agenda.js
@@ -1,8 +1,118 @@
 export const AGENDA_ITEMS = [
-  { time: '15:00', event: 'Apertura, acreditación y apertura stand' },
-  { time: '15:30', event: 'Charlas Bloque 1' },
-  { time: '17:00', event: 'Pausa · Stands · Networking' },
-  { time: '17:30', event: 'Charlas Bloque 2' },
-  { time: '19:30', event: 'Entrevistas y cierre de contenido' },
-  { time: '20:00', event: 'Cierre con música en vivo' }
+  {
+    time: '15:00',
+    title: 'Apertura de Puerta, música & networking y activaciones',
+    type: 'break'
+  },
+  {
+    time: '15:45',
+    title: 'Presentación BeOrange',
+    moderator: 'Yenifer Núñez de Her Chain',
+    speakers: [
+      'Maxi Perez de Plugin',
+      'Rodrigo Castañeda de Roderich’s Crypto House',
+      'Sebastian Contreras de Tangram'
+    ]
+  },
+  {
+    time: '16:00',
+    title: 'El Salvador desde Uruguay',
+    moderator: 'Nestor Rijo de Punta Bitcoin',
+    speakers: []
+  },
+  {
+    time: '16:10',
+    title: 'Ecosistema y Adopción',
+    moderator: 'Lucia Cardelino de Ethereum Uruguay',
+    speakers: [
+      'Bruno Vacotti de Acelerando Bitcoin',
+      'Martin Benitez de Cripto Guardian',
+      'Ivan Kaleja de B4H'
+    ]
+  },
+  {
+    time: '16:35',
+    title: 'RSK: Casos reales de uso',
+    moderator: 'Ignacio Varese de Blockchain Summit',
+    speakers: [
+      'Adrian Eidelman de RSK',
+      'Maximiliano Carjuza de Money On Chain',
+      'Jon Bravetti de BALT Protocol'
+    ]
+  },
+  {
+    time: '17:25',
+    title: 'Trivia y premios',
+    type: 'break'
+  },
+  {
+    time: '17:30',
+    title: 'BTC desde el sistema tradicional',
+    moderator: 'Maxi Perez de Plugin',
+    speakers: ['Nicolas Rodriguez de Club del Inversor']
+  },
+  {
+    time: '17:45',
+    title: 'Futuro del desarrollador',
+    moderator: 'Guido Iafigliola de Mutuo',
+    speakers: ['Juan Manuel Sobral de Space Dev']
+  },
+  {
+    time: '18:00',
+    title: 'Keynote - Bybit',
+    moderator: null,
+    speakers: ['Leandro Fiorenza de ByBit']
+  },
+  {
+    time: '18:15',
+    title: 'Minería',
+    moderator: 'Rodrigo Castañeda de Roderich’s Crypto House',
+    speakers: ['Fabrizio Bianchi de Hashrate Space']
+  },
+  {
+    time: '18:50',
+    title: 'Trivia y premios',
+    type: 'break'
+  },
+  {
+    time: '19:00',
+    title: 'Divulgando BTC',
+    moderator: 'Rodolfo Andragnes de Bitcoin.ar-Labitconf',
+    speakers: [
+      'Javier Puig de Archivo Cripto',
+      'Joy Koloff de BitcoinNea',
+      'Pamela Gonzales Perilli'
+    ]
+  },
+  {
+    time: '19:25',
+    title: 'Regulación',
+    moderator: 'Ricardo Mihura de Bitcoin AR',
+    speakers: [
+      'Carolina Rodriguez de Legaltech solutions',
+      'Juan Diana de Brum Costa Abogados',
+      'Ximena Gambaro'
+    ]
+  },
+  {
+    time: '19:50',
+    title: 'Entrevista BTC en LATAM',
+    moderator: 'Sebastian Contreras de Tangram',
+    speakers: ['Rodolfo Andragnes de Bitcoin.ar-LABITCONF']
+  },
+  {
+    time: '20:05',
+    title: 'Cierre y Sorteo',
+    moderator: 'Yenifer Núñez de Her Chain',
+    speakers: [
+      'Maxi Perez de Plugin',
+      'Rodrigo Castañeda de Roderich’s',
+      'Sebastian Contreras de Tangram'
+    ]
+  },
+  {
+    time: '20:15',
+    title: 'Música en vivo y Cocktail',
+    type: 'break'
+  }
 ]
